fab34de5e7ba8a148320cc21dfcad0d8043e7ac1
mkiselev <arz.kiselev@yandex.ru>
2017-04-20 19:33:06 +0300

    =payment

---
+++ models/UserTransaction.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace app\models;
+
+use Yii;
+
+/**
+ *
+ *
+ * @property integer $id
+ * @property integer $user_id
+ * @property double $amount
+ *
+ * @property User $user
+ */
+class UserTransaction extends \yii\db\ActiveRecord
+{
+    /**
+     * @inheritdoc
+     */
+    public static function tableName()
+    {
+        return 'user_transaction';
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function rules()
+    {
+        return [
+            [['user_id', 'amount'], 'required'],
+            [['user_id'], 'integer'],
+            [['amount'], 'number'],
+            [['user_id'], 'exist', 'skipOnError' => true, 'targetClass' => User::className(), 'targetAttribute' => ['user_id' => 'id']],
+        ];
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => 'ID',
+            'user_id' => 'User ID',
+            'amount' => 'Amount',
+        ];
+    }
+
+    /**
+     * @return \yii\db\ActiveQuery
+     */
+    public function getUser()
+    {
+        return $this->hasOne(User::className(), ['id' => 'user_id']);
+    }
+}
